b2a53f62c5f8d35a51fde108b61e1c135f1d305a
Mariano Cerrutti <vscorza@gmail.com>
2018-04-12 16:55:29 -0300

    adding parser_utils files

---
+++ src/parser_utils.c
@@ -0,0 +1,229 @@
+#include "parser_utils.h"
+#include "assert.h"
+
+void aut_dupstr(char** dst, char* src){
+	if(src != NULL){
+		*dst = malloc(strlen(src) + 1);
+		strcpy(*dst, src);
+	}else{
+		*dst = NULL;
+	}
+}
+
+automaton_expression_syntax* automaton_expression_create(automaton_expression_type_syntax type, automaton_expression_syntax* first
+		, automaton_expression_syntax* second, char* string_terminal, int32_t integer_terminal, automaton_expression_operator_syntax op){
+	automaton_expression_syntax* expr	= malloc(sizeof(automaton_expression_syntax));
+	expr->type	= type;
+	expr->first	= first;
+	expr->second= second;
+	aut_dupstr(&(expr->string_terminal), string_terminal);
+	expr->integer_terminal	= integer_terminal;
+	expr->op				= op;
+	return expr;
+}
+
+automaton_set_syntax* automaton_set_syntax_create(bool is_ident, uint32_t count, uint32_t* labels_count,
+		automaton_label_syntax*** labels, char* string_terminal){
+	automaton_set_syntax* set	= malloc(sizeof(automaton_set_syntax));
+	set->is_ident	= is_ident;
+	if(count > 0){
+		uint32_t i,j;
+		set->count			= count;
+		set->labels_count	= malloc(sizeof(uint32_t) * count);
+		for(i = 0; i < count; i++) set->labels_count[i]	= labels_count[i];
+		set->labels			= malloc(sizeof(automaton_label_syntax**) * count);
+		for(i = 0; i < count; i++){
+			set->labels[i]	= malloc(sizeof(automaton_label_syntax) * labels_count[i]);
+			for(j = 0; j < labels_count[i]; j++){
+				(set->labels[i])[j]	= labels[i][j];
+			}
+		}
+	}else{
+		set->count 			= 0;
+		set->labels_count	= NULL;
+		set->labels			= NULL;
+	}
+	aut_dupstr(&(set->string_terminal), string_terminal);
+	return set;
+}
+automaton_set_syntax* automaton_set_syntax_create_concurrent(char* string_terminal){
+	automaton_set_syntax* set	= malloc(sizeof(automaton_set_syntax));
+	set->is_ident	= false;
+	set->count			= 1;
+	set->labels_count	= malloc(sizeof(uint32_t) * set->count);
+	set->labels_count[0]= 1;
+	set->labels			= malloc(sizeof(automaton_label_syntax**) * set->count);
+	set->labels[0][0]	= automaton_label_syntax_create(false, NULL, string_terminal);
+	set->string_terminal= NULL;
+	return set;
+}
+
+automaton_set_syntax* automaton_set_syntax_concat_concurrent(automaton_set_syntax* set, char* string_terminal){
+	set->labels_count[0]++;
+	automaton_label_syntax** new_labels	= malloc(sizeof(automaton_label_syntax*) * set->labels_count[0]);
+	uint32_t i;
+	if(set->labels_count[0] > 1){
+		for(i = 0; i < (set->labels_count[0] - 1); i++){
+			new_labels[i]	= set->labels[0][i];
+		}
+	}
+	set->labels[0][set->labels_count[0] - 1]	= automaton_label_syntax_create(false, NULL, string_terminal);
+	free(set->labels[0][i]);
+	return set;
+}
+
+automaton_label_syntax* automaton_label_syntax_create(bool is_set, automaton_set_syntax* set, char* string_terminal){
+	automaton_label_syntax* label	= malloc(sizeof(automaton_label_syntax));
+	label->is_set	= is_set;
+	if(label->is_set){
+		label->set				= set;
+		label->string_terminal	= NULL;
+	}else{
+		label->set				= NULL;
+		aut_dupstr(&(label->string_terminal), string_terminal);
+	}
+	return label;
+}
+
+automaton_set_syntax* automaton_set_syntax_create_from_label(automaton_label_syntax* label){
+	automaton_set_syntax* set	= malloc(sizeof(automaton_set_syntax));
+	set->is_ident	= false;
+	set->count			= 1;
+	set->labels_count	= malloc(sizeof(uint32_t) * set->count);
+	set->labels_count[0]= 1;
+	set->labels			= malloc(sizeof(automaton_label_syntax*) * set->count);
+	set->labels[0][0]	= label;
+	set->string_terminal= NULL;
+	return set;
+}
+
+automaton_set_syntax* automaton_set_syntax_concat_labels(automaton_set_syntax* set, automaton_label_syntax* label){
+	set->labels_count[0]++;
+	automaton_label_syntax** new_labels	= malloc(sizeof(automaton_label_syntax*) * set->labels_count[0]);
+	uint32_t i;
+	if(set->labels_count[0] > 1){
+		for(i = 0; i < (set->labels_count[0] - 1); i++){
+			new_labels[i]	= set->labels[0][i];
+		}
+	}
+	set->labels[0][set->labels_count[0] - 1]	= label;
+	free(set->labels[0][i]);
+	return set;
+}
+
+automaton_set_syntax* automaton_set_syntax_create_from_ident(char* ident){
+	return automaton_set_syntax_create(true, 0, NULL, NULL, ident);
+}
+
+automaton_set_def_syntax* automaton_set_def_syntax_create(automaton_set_syntax* set, char* name){
+	automaton_set_def_syntax* set_def	= malloc(sizeof(automaton_set_syntax));
+	set_def->set		= set;
+	aut_dupstr(&(set_def->name), name);
+	return set_def;
+}
+
+automaton_fluent_syntax* automaton_fluent_syntax_create(char* name, automaton_set_syntax* initiating_set, automaton_set_syntax* finishing_set){
+	automaton_fluent_syntax* fluent	= malloc(sizeof(automaton_fluent_syntax));
+	fluent->initiating_set	= initiating_set;
+	fluent->finishing_set	= finishing_set;
+	aut_dupstr(&(fluent->name), name);
+	return fluent;
+}
+
+automaton_index_syntax* automaton_index_syntax_create(bool is_expr, bool is_range, automaton_expression_syntax* expr, char* lower_ident
+		, char* upper_ident){
+	automaton_index_syntax* index	= malloc(sizeof(automaton_index_syntax));
+
+	if(is_expr){
+		index->expr			= expr;
+		index->is_range		= false;
+		index->lower_ident	= NULL;
+		index->upper_ident	= NULL;
+	}else{
+		index->is_expr		= false;
+		aut_dupstr(&(index->lower_ident), lower_ident);
+		if(is_range){
+			index->is_range	= true;
+			index->expr		= expr;
+			index->upper_ident	= NULL;
+		}else{
+			index->is_range	= false;
+			index->expr		= NULL;
+			aut_dupstr(&(index->upper_ident), upper_ident);
+		}
+	}
+	return index;
+}
+
+automaton_indexes_syntax* automaton_indexes_syntax_create(automaton_index_syntax* first_index){
+	automaton_indexes_syntax* indexes	= malloc(sizeof(automaton_index_syntax));
+	indexes->count		= 1;
+	indexes->indexes	= malloc(sizeof(automaton_index_syntax*) * indexes->count);
+	indexes->indexes[0]	= first_index;
+	return indexes;
+}
+automaton_indexes_syntax* automaton_indexes_syntax_add_index(automaton_indexes_syntax* indexes, automaton_index_syntax* index){
+	indexes->count++;
+	automaton_index_syntax** new_indexes	= malloc(sizeof(automaton_index_syntax*) * indexes->count);
+	uint32_t i;
+	for(i = 0; i < (indexes->count-1); i++) new_indexes[i]	= indexes->indexes[i];
+	new_indexes[indexes->count-1]	= index;
+	free(indexes->indexes);
+	indexes->indexes	= new_indexes;
+	return indexes;
+}
+
+automaton_trace_label_atom_syntax* automaton_trace_label_atom_syntax_create_from_index(automaton_index_syntax* first_index){
+	automaton_trace_label_atom_syntax* label_atom	= malloc(sizeof(automaton_trace_label_atom_syntax));
+	label_atom->label	= NULL;
+	label_atom->indexes	= automaton_indexes_syntax_create(first_index);
+	return label_atom;
+}
+automaton_trace_label_atom_syntax* automaton_trace_label_atom_syntax_create(automaton_label_syntax* label, automaton_indexes_syntax* indexes){
+	automaton_trace_label_atom_syntax* label_atom	= malloc(sizeof(automaton_trace_label_atom_syntax));
+	label_atom->label	= label;
+	label_atom->indexes	= indexes;
+	return label_atom;
+}
+automaton_trace_label_syntax* automaton_trace_label_syntax_create(automaton_trace_label_atom_syntax* first_atom){
+	automaton_trace_label_syntax* trace_label		= malloc(sizeof(automaton_trace_label_syntax));
+	trace_label->count	= 1;
+	trace_label->atoms	= malloc(sizeof(automaton_trace_label_atom_syntax*) * trace_label->count);
+	trace_label->atoms[0]	= first_atom;
+	return trace_label;
+}
+automaton_trace_label_syntax* automaton_trace_label_syntax_add_atom(automaton_trace_label_syntax* trace_label
+		, automaton_trace_label_atom_syntax* atom){
+	trace_label->count++;
+	automaton_trace_label_atom_syntax** new_atoms	= malloc(sizeof(automaton_trace_label_atom_syntax*) * trace_label->count);
+	uint32_t i;
+	for(i = 0; i < (trace_label->count -1); i++) new_atoms[i]	= trace_label->atoms[i];
+	new_atoms[trace_label->count - 1]	= atom;
+	free(trace_label->atoms);
+	trace_label->atoms	= new_atoms;
+	return trace_label;
+}
+automaton_transition_syntax* automaton_transition_syntax_finish(automaton_expression_syntax* condition, automaton_transition_syntax* trace
+		, automaton_state_syntax* state){
+	trace->condition	= condition;
+	trace->to_state		= state;
+	return trace;
+}
+automaton_transition_syntax* automaton_transition_syntax_create_from_trace(automaton_trace_label_syntax* trace){
+	automaton_transition_syntax* transition	= malloc(sizeof(automaton_transition_syntax));
+	transition->count	= 1;
+	transition->condition	= NULL;
+	transition->to_state	= NULL;
+	transition->labels		= malloc(sizeof(automaton_trace_label_syntax*));
+	return transition;
+}
+automaton_transition_syntax* automaton_transition_syntax_add_trace(automaton_transition_syntax* transition, automaton_trace_label_syntax* trace){
+	transition->count++;
+	automaton_trace_label_syntax** new_labels		= malloc(sizeof(automaton_trace_label_syntax*) * transition->count);
+	uint32_t i;
+	for(i = 0; i < (transition->count -1); i++) new_labels[i]	= transition->labels[i];
+	new_labels[transition->count -1]	= trace;
+	free(transition->labels);
+	transition->labels	= new_labels;
+	return transition;
+}
---
+++ src/parser_utils.h
@@ -0,0 +1,138 @@
+#ifndef AUTOMATON_PARSER_UTILS_H
+#define AUTOMATON_PARSER_UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+/****************
+==== ENUMS ====
+****************/
+typedef enum {
+	RANGE_DEF_TYPE_AUT,
+	RANGE_TYPE_AUT,
+	CONST_TYPE_AUT,
+	BINARY_TYPE_AUT,
+	UNARY_TYPE_AUT,
+	PARENTHESIS_TYPE_AUT,
+	IDENT_TERMINAL_TYPE_AUT,
+	INTEGER_TERMINAL_TYPE_AUT,
+	UPPER_IDENT_TERMINAL_TYPE_AUT
+} automaton_expression_type_syntax;
+
+typedef enum {
+	NOP_AUT = 0,		// nop
+	RANGE_OP_AUT,		// ..
+	ASSIGN_OP_AUT,		// =
+	EQ_OP_AUT,			// ==
+	NEQ_OP_AUT,			// !=
+	GE_OP_AUT,			// >=
+	LE_OP_AUT,			// <=
+	GT_OP_AUT,			// >
+	LT_OP_AUT,			// <
+	PLUS_OP_AUT,		// +
+	MINUS_OP_AUT,		// -
+	MUL_OP_AUT,			// *
+	DIV_OP_AUT,			// /
+	MOD_OP_AUT,			// %
+	AND_OP_AUT,			// &
+	OR_OP_AUT,			// |
+	NOT_OP_AUT,			// !
+	XOR_OP_AUT,			// ^
+	DIAMOND_OP_AUT,		// <>
+	SQUARE_OP_AUT		// []
+} automaton_expression_operator_syntax;
+/****************
+==== STRUCTS ====
+*/
+typedef struct automaton_expression_syntax_str{
+	automaton_expression_type_syntax	type;
+	struct automaton_expression_syntax_str* first;
+	struct automaton_expression_syntax_str* second;
+	char* string_terminal;
+	int32_t integer_terminal;
+	automaton_expression_operator_syntax op;
+}automaton_expression_syntax;
+typedef struct automaton_label_syntax_str{
+	bool	is_set;
+	struct automaton_set_syntax_str* set;
+	char* string_terminal;
+}automaton_label_syntax;
+typedef struct automaton_set_syntax_str{
+	bool is_ident;
+	uint32_t count;
+	uint32_t* labels_count;
+	struct automaton_label_syntax_str*** labels;
+	char* string_terminal;
+}automaton_set_syntax;
+typedef struct automaton_set_def_syntax_str{
+	struct automaton_set_syntax_str* set;
+	char* name;
+}automaton_set_def_syntax;
+typedef struct automaton_fluent_syntax_str{
+	struct automaton_set_syntax_str* initiating_set;
+	struct automaton_set_syntax_str* finishing_set;
+	char* name;
+}automaton_fluent_syntax;
+typedef struct automaton_index_syntax_str{
+	bool is_expr;
+	bool is_range;
+	struct automaton_expression_syntax_str* expr;
+	char* lower_ident;
+	char* upper_ident;
+}automaton_index_syntax;
+typedef struct automaton_indexes_syntax_str{
+	uint32_t count;
+	struct automaton_index_syntax_str** indexes;
+}automaton_indexes_syntax;
+typedef struct automaton_state_syntax_str{
+	bool is_ref;
+	char* name;
+	struct automaton_state_syntax_str* ref;
+	uint32_t transitions_count;
+	struct automaton_transition_syntax_str** transitions;
+}automaton_state_syntax;
+typedef struct automaton_transition_syntax_str{
+	uint32_t count;
+	struct automaton_expression_syntax_str* condition;
+	struct automaton_trace_label_syntax_str** labels;
+	struct automaton_state_syntax_str* to_state;
+}automaton_transition_syntax;
+typedef struct automaton_trace_label_syntax_str{
+	uint32_t count;
+	struct automaton_trace_label_atom_syntax_str** atoms;
+}automaton_trace_label_syntax;
+typedef struct automaton_trace_label_atom_syntax_str{
+	struct automaton_label_syntax_str* label;
+	struct automaton_indexes_syntax_str* indexes;
+}automaton_trace_label_atom_syntax;
+/****************
+==== STRUCTS ====
+*/
+automaton_expression_syntax* automaton_expression_syntax_create(automaton_expression_type_syntax type, automaton_expression_syntax* first
+		, automaton_expression_syntax* second, char* string_terminal, int32_t integer_terminal, automaton_expression_operator_syntax op);
+automaton_set_syntax* automaton_set_syntax_create(bool is_ident, uint32_t count, uint32_t* labels_count,
+		automaton_label_syntax*** labels, char* string_terminal);
+automaton_set_syntax* automaton_set_syntax_create_concurrent(char* string_terminal);
+automaton_set_syntax* automaton_set_syntax_concat_concurrent(automaton_set_syntax* set, char* string_terminal);
+automaton_set_syntax* automaton_set_syntax_create_from_label(automaton_label_syntax* label);
+automaton_set_syntax* automaton_set_syntax_create_from_ident(char* ident);
+automaton_set_syntax* automaton_set_syntax_concat_labels(automaton_set_syntax* set, automaton_label_syntax* label);
+automaton_label_syntax* automaton_label_syntax_create(bool is_set, automaton_set_syntax* set, char* string_terminal);
+automaton_set_def_syntax* automaton_set_def_syntax_create(automaton_set_syntax* set, char* name);
+automaton_fluent_syntax* automaton_fluent_syntax_create(char* name, automaton_set_syntax* initiating_set, automaton_set_syntax* finishing_set);
+automaton_index_syntax* automaton_index_syntax_create(bool is_expr, bool is_range, automaton_expression_syntax* expr, char* lower_ident
+		, char* upper_ident);
+automaton_indexes_syntax* automaton_indexes_syntax_create(automaton_index_syntax* first_index);
+automaton_indexes_syntax* automaton_indexes_syntax_add_index(automaton_indexes_syntax* indexes, automaton_index_syntax* index);
+automaton_trace_label_atom_syntax* automaton_trace_label_atom_syntax_create_from_index(automaton_index_syntax* first_index);
+automaton_trace_label_atom_syntax* automaton_trace_label_atom_syntax_create(automaton_label_syntax* label, automaton_indexes_syntax* indexes);
+automaton_trace_label_syntax* automaton_trace_label_syntax_create(automaton_trace_label_atom_syntax* first_atom);
+automaton_trace_label_syntax* automaton_trace_label_syntax_add_atom(automaton_trace_label_syntax* trace_label, automaton_trace_label_atom_syntax* atom);
+automaton_transition_syntax* automaton_transition_syntax_finish(automaton_expression_syntax* condition, automaton_transition_syntax* trace
+		, automaton_state_syntax* state);
+automaton_transition_syntax* automaton_transition_syntax_create_from_trace(automaton_trace_label_syntax* trace);
+automaton_transition_syntax* automaton_transition_syntax_add_trace(automaton_transition_syntax* transition, automaton_trace_label_syntax* trace);
+#endif
