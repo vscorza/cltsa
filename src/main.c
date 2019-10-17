b910004f1cc08e3ecaa8d0d6252f0dcd9ee57ae7
Mariano Cerrutti <vscorza@gmail.com>
2019-10-17 17:04:02 -0300

    updating fluent valuation appliance in code and defs

--- src/main.c
+++ src/main.c
@@ -1139,12 +1139,12 @@ int main (int argc, char** argv){
 		}
 	}else{
 		//ONGOING
-		//run_parse_test("tests/genbuf_2_sndrs.fsp", "GenBuf 2 sndrs");//GENBUF 2 Sndrs
+		run_parse_test("tests/genbuf_2_sndrs.fsp", "GenBuf 2 sndrs");//GENBUF 2 Sndrs
 
 
 		//GENERAL TESTS
 		//run_all_tests();
-		run_functional_tests();
+		//run_functional_tests();
 
 
 		//TODO
