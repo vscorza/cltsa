9c39c732b76907c71f43cdd5868a0834cf913810
Mariano Cerrutti <mariano.cerrutti@celmatix.com>
2020-02-26 15:07:11 -0300

    adding simplified version of ahb

--- src/main.c
+++ src/main.c
@@ -1148,12 +1148,13 @@ int main (int argc, char** argv){
 		//run_parse_test("tests/automata_load_test_1.fsp",  "automata load test");
 		//run_parse_test("tests/automata_load_test_2.fsp",  "automata load test");
 		//run_parse_test("diagnoses/ahb/ahb_2_masters/ahb_2_masters_v1.fsp", "AHB 2 Masters");//AHB 2 Masters
+		run_parse_test("diagnoses/ahb/ahb_2_masters/ahb_2_masters_simplified_v1.fsp", "AHB 2 Masters");//AHB 2 Masters
 		//run_parse_test("diagnoses/syntcomp/collector/collector_2_in_v1.fsp", "Collector 2 inputs");//Collector 2 inputs
 		//run_parse_test("diagnoses/syntcomp/collector/collector_3_in_v1.fsp", "Collector 3 inputs");//Collector 3 inputs
 		//run_parse_test("diagnoses/syntcomp/collector/collector_4_in_v1.fsp", "Collector 4 inputs");//Collector 4 inputs
 		//run_parse_test("diagnoses/syntcomp/collector/collector_5_in_v1.fsp", "Collector 5 inputs");//Collector 5 inputs
 		//run_parse_test("diagnoses/syntcomp/collector/collector_6_in_v3.fsp", "Collector 6 inputs");//Collector 6 inputs
-		run_parse_test("diagnoses/syntcomp/collector/collector_9_in_v1.fsp", "Collector 9 inputs");//Collector 6 inputs
+		//run_parse_test("diagnoses/syntcomp/collector/collector_9_in_v1.fsp", "Collector 9 inputs");//Collector 6 inputs
 		//run_parse_test("diagnoses/syntcomp/collector/collector_3_in_missing_assumption_v1.fsp", "Collector 3 inputs");//Collector 3 inputs missing 1 assumtpion
 		//run_parse_test("diagnoses/syntcomp/collector/collector_4_in_missing_assumption_v1.fsp", "Collector 4 inputs");//Collector 4 inputs missing 1 assumption
 
