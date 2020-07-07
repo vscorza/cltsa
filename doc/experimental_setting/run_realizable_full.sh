9187ce662626f62c616e513718aeccbc87750c3f
Mariano Cerrutti <mariano.cerrutti@celmatix.com>
2020-07-07 12:24:21 -0300

    updating scripts to run both in host and docker

--- doc/experimental_setting/run_realizable_full.sh
+++ doc/experimental_setting/run_realizable_full.sh
@@ -2,12 +2,12 @@
 # run with params: dst_dir, output_dir, cltsa_dir, max_iters
 echo "[[Running realizable examples]]"
 # max it. 4
-./run_realizable_lift_controller_1_n.sh $1 $2 $3 1 $4 
+./run_realizable_lift_controller_1_n.sh $1 $2 $3 4 $4 
 # max it. 5
-./run_realizable_collector_1_n.sh $1 $2 $3 1 $4
+./run_realizable_collector_1_n.sh $1 $2 $3 5 $4
 # max it. 9
-./run_realizable_exploration_robot_1_n.sh $1 $2 $3 1 $4
+./run_realizable_exploration_robot_1_n.sh $1 $2 $3 9 $4
 # max it. 3
-./run_realizable_genbuf_1_n.sh $1 $2 $3 1 $4
+./run_realizable_genbuf_1_n.sh $1 $2 $3 3 $4
 
 
